a786814569bd39d42dcb556488c22d3fa7439169
siripr1 <rajsir@gmail.com>
2023-07-07 20:22:42 -0700

    testing - adding new resource

--- rajendhar-project01/Project01-Demo/10_ResourceCode/06_redis.tf
+++ rajendhar-project01/Project01-Demo/10_ResourceCode/06_redis.tf
@@ -1,9 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "azurerm" {
-  features {}
-}
+
 
 resource "azurerm_resource_group" "raj-example" {
   name     = "${var.prefix}-resources"
@@ -11,10 +9,10 @@ resource "azurerm_resource_group" "raj-example" {
 }
 
 #--- NOTE: the Name used for Redis needs to be globally unique
-resource "azurerm_redis_cache" "raj-example" {
+resource "azurerm_redis_cache" "raj-example-1" {
   name                = "${var.prefix}-cache"
-  location            = azurerm_resource_group.create-rg.location
-  resource_group_name = azurerm_resource_group.create-rg.name
+  location            = azurerm_resource_group.raj-example.location
+  resource_group_name = azurerm_resource_group.raj-example.name
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"
--- rajendhar-project01/Project01-Demo/10_ResourceCode/variable.tf
+++ rajendhar-project01/Project01-Demo/10_ResourceCode/variable.tf
@@ -1,7 +1,10 @@
 variable "prefix" {
   description = "The Prefix used for all resources in this example"
+  default = "raj-az-exmp"
+
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "uksouth"
 }
